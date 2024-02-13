389590f0be32d2eb4a7d3eaa13059a1276fb4018
Vladislav Andreev <andreevvlad443@gmail.com>
2024-02-13 15:53:53 +0300

    feat: error isntance stringification method

--- errors.py
+++ errors.py
@@ -4,6 +4,10 @@ class FileError(Exception):
   """
   def __init__(self, message) -> None:
     super().__init__("File error occured: " + message)
+    self.message = "File error occured: " + message
+
+  def __str__(self) -> str:
+    return self.message
 
 class MatrixError(Exception):
   """
@@ -11,3 +15,7 @@ class MatrixError(Exception):
   """
   def __init__(self, message) -> None:
     super().__init__("Matrix error occured: " + message)
+    self.message = "Matrix error occured: " + message
+  
+  def __str__(self) -> str:
+    return self.message
